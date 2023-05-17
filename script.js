843c007c9431b4aee3417351ceba371559958542
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-05-17 21:34:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,16 +10,89 @@ function calculateMinCost() {
 	let text = document.getElementById("rope-lengths").value;
 	let result = document.getElementById("result");
 	let arr = text.split(",")
-	let sortedArray = arr.map(Number).sort();
-	let arr2 = [...sortedArray]
-    let sum = 0, minCost = 0;
-    for(let i = 0; i < sortedArray.length; i++){
-        arr2 = arr2.sort((a,b)=>a-b);
-        let a = arr2.shift();
-        let b = arr2.shift();
-        sum = a + b;
-        if(sum) minCost += sum
-        arr2.push(sum);
-    }
-	result.innerHTML = minCost
+	arr = arr.map(Number);
+	 if (arr.length <= 1) {
+	    result.innerHTML = ans
+		 return;
+	  }
+
+  let ans = 0;
+  const pq = new MinHeap();
+
+  for (let i = 0; i < n; i++) {
+	pq.insert(arr[i]);
+  }
+
+  while (!pq.isEmpty()) {
+	const first = pq.extractMin();
+	const second = pq.extractMin();
+	const temp = first + second;
+	ans += temp;
+	if (!pq.isEmpty()) {
+	  pq.insert(temp);
+	}
+  }
+	result.innerHTML = ans
 }  
+
+// MinHeap class
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    let currentIndex = this.heap.length - 1;
+
+    while (currentIndex > 0) {
+      const parentIndex = Math.floor((currentIndex - 1) / 2);
+
+      if (this.heap[currentIndex] < this.heap[parentIndex]) {
+        [this.heap[currentIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currentIndex]];
+        currentIndex = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  extractMin() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+
+    const minValue = this.heap[0];
+    const lastValue = this.heap.pop();
+
+    if (this.heap.length > 0) {
+      this.heap[0] = lastValue;
+      this.heapify(0);
+    }
+
+    return minValue;
+  }
+
+  heapify(index) {
+    const leftChildIndex = 2 * index + 1;
+    const rightChildIndex = 2 * index + 2;
+    let smallestIndex = index;
+
+    if (leftChildIndex < this.heap.length && this.heap[leftChildIndex] < this.heap[smallestIndex]) {
+      smallestIndex = leftChildIndex;
+    }
+
+    if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[smallestIndex]) {
+      smallestIndex = rightChildIndex;
+    }
+
+    if (smallestIndex !== index) {
+      [this.heap[index], this.heap[smallestIndex]] = [this.heap[smallestIndex], this.heap[index]];
+      this.heapify(smallestIndex);
+    }
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+}
