89ad40a1ff44288630a4d5ce600c246b65eb4fa9
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-05-17 16:26:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+	let text = document.getElementById("rope-lengths").value;
+	let ans = document.getElementById("result");
+	let arr = text.split(",")
+	ans.innerHTML = arr.reduce(() => {
+		
+	})	
+/**
+	[4, 2, 7, 6, 9]
+	[6, 7, 6, 9]
+	[6, 13, 6, 9]
+	[6, 13, 19, 9]
+	[6, 13, 19, 28]
+	sum = 6 + 13 + 19 + 28 =  
+*/
 }  
